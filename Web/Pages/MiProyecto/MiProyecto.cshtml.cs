26ac415744408f0c39f865ff6eef4dcc6a97bb3e
Marcio Abriola <marcioabriola@gmail.com>
2022-08-27 13:01:01 -0300

    Agregar archivos de proyecto.

---
+++ Web/Pages/MiProyecto/MiProyecto.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace Web.Pages.MiProyecto
+{
+    public class MiProyectoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
