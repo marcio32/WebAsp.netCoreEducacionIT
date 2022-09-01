68b708267ecf356786c21b8b8f597f6781b7546c
Marcio Abriola <marcioabriola@gmail.com>
2022-08-31 21:58:17 -0300

    Clase 6 - Se crea el inicio, usuarios con su table y se reacomoda el proyecto

---
+++ Web/wwwroot/js/Usuarios/Usuarios.js
@@ -0,0 +1,7 @@
+﻿$(document).ready(function () {
+    $('#usuarios').DataTable({
+        languaje: {
+            url:"https://cdn.datatables.net/plug-ins/1.10.19a/i18n/Spanish.json"
+        }
+    });
+});
