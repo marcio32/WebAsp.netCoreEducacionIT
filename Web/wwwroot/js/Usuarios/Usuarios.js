a5232ee5ab9cf9b78dec6dc3304b038f7a4afd1f
Marcio Abriola <marcioabriola@gmail.com>
2022-09-02 22:00:14 -0300

    Se sube diseño de layout mas consulta a la api random

--- Web/wwwroot/js/Usuarios/Usuarios.js
+++ Web/wwwroot/js/Usuarios/Usuarios.js
@@ -1,7 +1,32 @@
 ﻿$(document).ready(function () {
     $('#usuarios').DataTable({
+        ajax: {
+            url: 'https://randomuser.me/api/',
+            dataSrc: "results"
+        },
+        columns: [
+            {data: 'name.first', title:'Nombre'},
+            {data: 'name.last', title:'Apellido'},
+            { data: 'location.country', title:'Pais'},
+        ],
         languaje: {
             url:"https://cdn.datatables.net/plug-ins/1.10.19a/i18n/Spanish.json"
         }
     });
 });
+
+
+$('#botonpulsado').on('click', function () {
+    $.ajax({
+        url: 'https://randomuser.me/api/',
+        dataType: 'json',
+        success: function (data) {
+            debugger
+            console.log(data);
+        }
+    });
+
+})
+
+
+
